a1912fa85251d3b6515a4797dd439a6546c9d6ca
Luke Hammer <textchimp@gmail.com>
2020-07-09 17:09:58 +1000

    objects exercises and shakespearean insults review

---
+++ week1/shakespearean-insults/js/main.js
@@ -0,0 +1,209 @@
+
+console.log('Shakespearean Insults!');
+
+// Our arrays of insult adjectives and nouns:
+const firstAdjectives = [
+  'artless',
+  'bawdy',
+  'beslubbering',
+  'bootless',
+  'churlish',
+  'cockered',
+  'clouted',
+  'craven',
+  'currish',
+  'dankish',
+  'dissembling',
+  'droning',
+  'errant',
+  'fawning',
+  'fobbing',
+  'froward',
+  'frothy',
+  'gleeking',
+  'goatish',
+  'gorbellied',
+  'impertinent',
+  'infectious',
+  'jarring',
+  'loggerheaded',
+  'lumpish',
+  'mammering',
+  'mangled',
+  'mewling',
+  'paunchy',
+  'pribbling',
+  'puking',
+  'puny',
+  'qualling',
+  'rank',
+  'reeky',
+  'roguish',
+  'ruttish',
+  'saucy',
+  'spleeny',
+  'spongy',
+  'surly',
+  'tottering',
+  'unmuzzled',
+  'vain',
+  'venomed',
+  'villainous',
+  'warped',
+  'wayward',
+  'weedy',
+  'yeasty'
+];
+
+const secondAdjectives = [
+  'base-court',
+  'bat-fowling',
+  'beef-witted',
+  'beetle-headed',
+  'boil-brained',
+  'clapper-clawed',
+  'clay-brained',
+  'common-kissing',
+  'crook-pated',
+  'dismal-dreaming',
+  'dizzy-eyed',
+  'doghearted',
+  'dread-bolted',
+  'earth-vexing',
+  'elf-skinned',
+  'fat-kidneyed',
+  'fen-sucked',
+  'flap-mouthed',
+  'fly-bitten',
+  'folly-fallen',
+  'fool-born',
+  'full-gorged',
+  'guts-griping',
+  'half-faced',
+  'hasty-witted',
+  'hedge-born',
+  'hell-hated',
+  'idle-headed',
+  'ill-breeding',
+  'ill-nurtured',
+  'knotty-pated',
+  'milk-livered',
+  'motley-minded',
+  'onion-eyed',
+  'plume-plucked',
+  'pottle-deep',
+  'pox-marked',
+  'reeling-ripe',
+  'rough-hewn',
+  'rude-growing',
+  'rump-fed',
+  'shard-borne',
+  'sheep-biting',
+  'spur-galled',
+  'swag-bellied',
+  'tardy-gaited',
+  'tickle-brained',
+  'toad-spotted',
+  'unchin-snouted',
+  'weather-bitten'
+];
+
+const nouns = [
+  'apple-john',
+  'baggage',
+  'barnacle',
+  'bladder',
+  'boar-pig',
+  'bugbear',
+  'bum-bailey',
+  'canker-blossom',
+  'clack-dish',
+  'clotpole',
+  'coxcomb',
+  'codpiece',
+  'death-token',
+  'dewberry',
+  'flap-dragon',
+  'flax-wench',
+  'flirt-gill',
+  'foot-licker',
+  'fustilarian',
+  'giglet',
+  'gudgeon',
+  'haggard',
+  'harpy',
+  'hedge-pig',
+  'horn-beast',
+  'hugger-mugger',
+  'joithead',
+  'lewdster',
+  'lout',
+  'maggot-pie',
+  'malt-worm',
+  'mammet',
+  'measle',
+  'minnow',
+  'miscreant',
+  'moldwarp',
+  'mumble-news',
+  'nut-hook',
+  'pigeon-egg',
+  'pignut',
+  'puttock',
+  'pumpion',
+  'ratsbane',
+  'scut',
+  'skainsmate',
+  'strumpet',
+  'varlot',
+  'vassal',
+  'whey-face',
+  'wagtail'
+];
+
+// PSEUDOCODE
+// Goal: print out a randomly-generated insult
+
+//  1. Pick a random first adjective from the first array
+//     -> Pick a random element from an array???
+//        a. Generate and store a random number between 0 and .length - 1
+//        b. Use that random number as an index into the array
+//        c. Store whatever is returned from that index
+
+//  2. Pick a random second adjective from the second array
+//  3. Pick a random noun from the third array
+//  4. Concatenate (join) them together with spaces and print out (with Thou at the front)
+
+
+// generateRandomInt( max )  : return a random integer in the range 0..max-1 (non-inclusive)
+
+const generateRandomInt = function( max ){
+  const randomNum = Math.random() * max;
+  const randomInt = Math.floor( randomNum );
+  return randomInt;
+
+  // one-liner
+  // return Math.floor( Math.random() * max );
+
+}; // generateRandomInt()
+
+
+// getRandomElementFromArray( array ) : return a random element from the specified array
+
+const getRandomElementFromArray = function( array ){
+  const randomIndex = generateRandomInt( array.length );
+  const randomItem = array[ randomIndex ];
+  return randomItem;
+}; // getRandomElementFromArray()
+
+
+
+// generateInsult() : generate and print a random insult
+
+const generateInsult = function() {
+  const firstAdjective = getRandomElementFromArray( firstAdjectives );
+  const secondAdjective = getRandomElementFromArray( secondAdjectives );
+  const noun = getRandomElementFromArray( nouns );
+
+  console.log(`Thou ${firstAdjective} ${secondAdjective} ${noun}!`);
+};

dc59c349fa5f37187f38a9f1fa10044c9c99c3bd
Luke Hammer <textchimp@gmail.com>
2020-08-05 15:50:57 +1000

    finished CRUD for Artist

--- week5/moma-app/app/controllers/artists_controller.rb
+++ week5/moma-app/app/controllers/artists_controller.rb
@@ -1,6 +1,20 @@
 class ArtistsController < ApplicationController
 
+  # CREATE ###########################
 
+  # 1. Blank form
+  def new
+    # Create a new empty object to give to 'form_for' in the template
+    # - this object does not exist yet in the database! Just in Ruby memory
+    @artist = Artist.new
+  end
+
+  # 2. Form submit, create, redirect
+  def create
+    # TODO: check for validation errors? Does .create return an object, or false?
+    Artist.create artist_params # use strong params to filter the form fields being saved
+    redirect_to artists_path   # no template, go back to the index '/artists'
+  end
 
   # READ #############################
 
@@ -14,7 +28,44 @@ class ArtistsController < ApplicationController
     @artist = Artist.find params[:id]
   end
 
+  # UPDATE ######################################
 
+  # 1. Pre-filled edit form
+  def edit
+    @artist = Artist.find params[:id]
+  end
+
+  # 2. Form submit, perform update, redirect to show page
+  def update
+    artist = Artist.find params[:id]
+
+    # Use the same strong params method we used in the create action, for this update:
+    artist.update artist_params
+
+    # No template for updates; redirect to the show page (using the path helper)
+    redirect_to( artist_path(params[:id]) )
+
+  end # update
+
+  # DESTROY #######################################
+
+  def destroy
+    Artist.destroy params[:id]
+    redirect_to artists_path
+  end
+
+
+
+
+  private
+  # ^ private means the following methods (until the end of the class)
+  #   are NOT actions, i.e. they do not correspond to routes
+
+  # Strong params! Acts like a doorman with a door list, only letting through
+  # the specified form param fields, so we can add them to our table
+  def artist_params
+    params.require(:artist).permit( :name, :nationality, :dob, :period, :roundness, :bio, :image )
+  end
 
 
 end  # ArtistsController
--- week5/moma-app/config/routes.rb
+++ week5/moma-app/config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   # Artists CRUD
 
   # CREATE
+
   # 1. Blank form
+  get '/artists/new' => 'artists#new', as: 'new_artist'
+  # this is what Rails will give us later when we use the one-line CRUD route helper
+
   # 2. Form submit, create, redirect to index
+  post '/artists' => 'artists#create'   # path helper will be 'arists_path', same as index
 
   # READ
 
@@ -17,10 +22,13 @@ Rails.application.routes.draw do
 
   # UPDATE
   # 1. Pre-filled form for a specific artist by ID
+  get '/artists/:id/edit' => 'artists#edit', as: 'edit_artist'
+
   # 2. Form submit, update artist, redirect to show
+  patch '/artists/:id' => 'artists#update'
 
   # DESTROY
   # Destroy artist row by ID, redirect to index
-
+  delete '/artists/:id' => 'artists#destroy'
 
 end
